ccc0c600358807617f75b8b0b509c707051a1b39
Acheron-o <caio.dev.person@gmail.com>
2025-08-16 12:25:01 -0300

    Turned the project to English and started the first step

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//The main goal of this challenge is to improve your own abilities in programming log. Here you must develop the logic to solve the problem
+let friend = []
+//Create a function that stores the friend's name when the "ADD" button is clicked
