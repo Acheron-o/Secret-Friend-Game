5d2b58da7e2a0feb3e738b88f275e0ee8cd064ff
Acheron-o <caio.dev.person@gmail.com>
2025-08-20 12:13:28 -0300

    Function that store friend's name added.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 //The main goal of this challenge is to improve your own abilities in programming log. Here you must develop the logic to solve the problem
-let friend = []
+let typedFriends = []
 //Create a function that stores the friend's name when the "ADD" button is clicked
+function addFriend() {
+    let friendName = document.querySelector('input').value;
+    typedFriends.push(friendName);
+    document.querySelector('input').value = '';
+    console.log(typedFriends);
+}
