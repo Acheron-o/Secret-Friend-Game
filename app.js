e2211f66d1748f9bb43b2f4671f8489222bba173
Acheron-o <caio.dev.person@gmail.com>
2025-08-20 17:17:39 -0300

    Fução para agregar os nomes dos amigos

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Crie um array para armazenar os nomes
+let jogadores = [];
+//Implementa uma função para agregar amigos
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+   jogadores.push(nome);
+   //Verificar os nomes no console
+   console.log(jogadores);
+
+}
