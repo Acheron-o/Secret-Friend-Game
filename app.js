6cdc3b9db6ab0313f57acb605229365ea0eec052
Acheron-o <caio.dev.person@gmail.com>
2025-08-20 17:08:59 -0300

    Resetando o projeto

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-//The main goal of this challenge is to improve your own abilities in programming log. Here you must develop the logic to solve the problem
-let typedFriends = []
-//Create a function that stores the friend's name when the "ADD" button is clicked
-function addFriend() {
-    let friendName = document.querySelector('input').value;
-    typedFriends.push(friendName);
-    document.querySelector('input').value = '';
-    console.log(typedFriends);
-}
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
