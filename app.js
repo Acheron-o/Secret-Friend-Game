23ce3a7efdf53448a4c4ac6ad9fc5d5dac5821db
Acheron-o <caio.dev.person@gmail.com>
2025-08-21 00:14:16 -0300

    Finalizações do codigo

--- app.js
+++ app.js
@@ -6,15 +6,34 @@
 let jogadores = [];
 //Implementa uma função para agregar amigos
 function adicionarAmigo() {
-    let nome = document.getElementById('amigo').value;
+    const input = document.getElementById('amigo');
+    let nome = input.value.trim();
+    //Verifica se o campo de entrada está vazio
+    if (nome === "") { 
+               document.querySelector("h2").innerHTML = "Digite um nome válido"
+
+        return;
+    } 
+        document.querySelector("h2").innerHTML = "Digite o nome dos seus amigos";
+
    jogadores.push(nome);
    //Verificar os nomes no console
    console.log(jogadores);
    //limpar o campo de entrada
    nome = document.querySelector("input");
     nome.value = "";
+   //Colocara os nomes na tela
+   // Atualiza a lista de amigos na tela
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; // Limpa a lista antes de atualizar
+    jogadores.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
 
-}
+    });
+resultado.innerHTML = "";}
+
 //Adiciona uma função para que o resultado seja totalmente aleatório
 function amigoAleatorio(totalAmigos) {
   return Math.floor(Math.random() * totalAmigos);
@@ -27,10 +46,24 @@ function sortearAmigo() {
         
     }
    else {
-        let resultado = amigoAleatorio(jogadores.length);
-         document.querySelector("h2").innerHTML = "Digite o nome dos seus amigos"
-    alert(jogadores[resultado]); // um alerta inicial para teste
+        let resultadoSorteio = amigoAleatorio(jogadores.length);
+    const resultados = jogadores[resultadoSorteio];
+
+    // Mostra o resultado na tela
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = ""; // Limpa resultados anteriores
+    const li = document.createElement('li');
+    li.textContent = resultados
+    resultado.appendChild(li);
+
+    // Limpa lista de amigos
+    jogadores = [];
+    document.getElementById('listaAmigos').innerHTML = "";
+
+     document.querySelector("h2").innerHTML = "Sorteio realizado com sucesso!";    
         }
     
     
 }
+
+
